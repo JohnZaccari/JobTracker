c31c22726a922f678fc0fed70605e623387264d6
John Zaccari <john@Johns-MBP.home>
2017-10-27 23:04:21 -0400

    Add customer List

--- Customers.js
+++ Customers.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View, Component } from 'react-native';
+import { StyleSheet, Text, View, Component, NavigatorIOS } from 'react-native';
+import CustomersList from './CustomersList.js';
 
 export default class Customers extends React.Component {
   render() {
     return (
-  	  <View style={styles.container}>
-        <Text style={styles.description}>
-        	Customers Tab
-        </Text>
-      </View>
+	    <NavigatorIOS style={styles.container}
+      initialRoute={{
+        title: "Customers",
+        component: CustomersList
+      }}/>
+
     );
   }
 }
 
-var styles = StyleSheet.create({
-    description: {
-        fontSize: 20,
-        backgroundColor: 'white'
-    },
+const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+        flex: 1
     }
 });
