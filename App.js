d51484f169d6cc73deab5cfab74fbdd5b90e7a08
Michael Zaccari <michael.zaccari@accelerated.com>
2017-10-27 22:06:06 -0400

    Latest changes

--- App.js
+++ App.js
@@ -1,15 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TabBarIOS } from 'react-native';
 import Featured from './Featured';
 import Search from './Search';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { selectedTab: 'featured' };
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Job Index</Text>
-        <Text>Test Conflict</Text>
-      </View>
+      <TabBarIOS selectedTab={this.state.selectedTab}>
+               <TabBarIOS.Item
+                   selected={this.state.selectedTab === 'featured'}
+
+                   onPress={() => {
+                       this.setState({
+                           selectedTab: 'featured'
+                       });
+                   }}>
+                   <Featured/>
+               </TabBarIOS.Item>
+               <TabBarIOS.Item selected={this.state.selectedTab === 'search'}
+                   onPress={() => {
+                       this.setState({
+                           selectedTab: 'search'
+                       });
+                   }}>
+                   <Search/>
+               </TabBarIOS.Item>
+           </TabBarIOS>
     );
   }
 }
