8311b0f900c36e81abf0357ebab9a483ed75e115
Michael Zaccari <michael.zaccari@accelerated.com>
2017-10-27 21:09:43 -0400

    Another change

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Text>Job Index</Text>
+        <Text>Test Conflict</Text>
       </View>
     );
   }
